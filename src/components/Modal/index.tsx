4547867e33d4e1f3ea0bf432d7052c35727f949c
denbarabraza <denis.bareischev@gmail.com>
2023-05-18 00:06:40 +0300

    feat: create new API (selectedFilmAPI)

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, memo, ReactNode } from 'react';
+import styled from 'styled-components';
 
 import s from './Modal.module.css';
 
@@ -20,3 +21,20 @@ export const Modal: FC<IModal> = memo(({ children, onClose, isOpen }) => {
     </div>
   );
 });
+
+export const ModalItem = styled.div<{ isOpen: boolean }>`
+  pointer-events: ${({ isOpen }) => (isOpen ? 'all' : 'none')};
+  position: ${({ isOpen }) => (isOpen ? 'fixed' : '')};
+  top: ${({ isOpen }) => (isOpen ? 0 : '')};
+  left: ${({ isOpen }) => (isOpen ? 0 : '')};
+  display: ${({ isOpen }) => (isOpen ? 'flex' : '')};
+  align-items: ${({ isOpen }) => (isOpen ? 'center' : '')};
+  justify-content: ${({ isOpen }) => (isOpen ? 'center' : '')};
+  width: ${({ isOpen }) => (isOpen ? '100vw' : '')};
+  height: ${({ isOpen }) => (isOpen ? '100vw' : '')};
+  opacity: ${({ isOpen }) => (isOpen ? 1 : '0')};
+  background-color: ${({ isOpen }) => (isOpen ? '#0000000A' : '')};
+  transition: ${({ isOpen }) => (isOpen ? '0.4s' : '')};
+
+  transform: ${({ isOpen }) => (isOpen ? '' : 'scale(0.5)')};
+`;
--- src/store/services/filmsService.ts
+++ src/store/services/filmsService.ts
@@ -2,16 +2,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { IFilms } from '@/interfaces/films';
 
-const API_KEY = process.env.X_RAPIDAPI_KEY;
+const API_KEY_RAPID = process.env.X_RAPIDAPI_KEY;
+const API_KEY_TMDB = process.env.X_TMDBAPI_KEY;
 
 export const filmsAPI = createApi({
-  reducerPath: 'moviesAPI',
+  reducerPath: 'filmsAPI',
   tagTypes: ['Films'],
   keepUnusedDataFor: 86400,
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://ott-details.p.rapidapi.com/',
     headers: {
-      'X-RapidAPI-Key': API_KEY,
+      'X-RapidAPI-Key': API_KEY_RAPID,
       'X-RapidAPI-Host': 'ott-details.p.rapidapi.com',
     },
   }),
@@ -24,7 +25,7 @@ export const filmsAPI = createApi({
       providesTags: ['Films'],
     }),
     fetchByTitleFilms: builder.query<IFilms, string | null>({
-      query: (title: string) => ({
+      query: title => ({
         url: 'search',
         params: { title },
       }),
@@ -33,4 +34,27 @@ export const filmsAPI = createApi({
   }),
 });
 
+export const selectedFilmAPI = createApi({
+  reducerPath: 'selectedFilmAPI',
+  tagTypes: ['SelectedFilm'],
+  keepUnusedDataFor: 86400,
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3/movie',
+  }),
+  endpoints: builder => ({
+    fetchSelectedFilm: builder.query<IFilms, string>({
+      query: filmID => ({
+        url: `${filmID}`,
+        params: {
+          api_key: API_KEY_TMDB,
+          external_source: 'imdb_id',
+          append_to_response: 'videos',
+        },
+      }),
+      providesTags: ['SelectedFilm'],
+    }),
+  }),
+});
+
 export const { useFetchFilmsQuery, useFetchByTitleFilmsQuery } = filmsAPI;
+export const { useFetchSelectedFilmQuery } = selectedFilmAPI;
